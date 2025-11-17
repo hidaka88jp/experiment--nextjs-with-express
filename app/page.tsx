9318d3c5618a134d3008ab5a71ac465fa354307c
hidaka88jp <hidaka88jp@yahoo.co.jp>
2025-11-17 11:05:32 -0800

    feat: login page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <p>my page</p>
+    <>
+      <p>my page</p>
+      <Link href="/login">Login</Link>
+    </>
   );
 }
