a5bc6280c1c8050d34b1b8bf8ab57822c135b96b
hidaka88jp <hidaka88jp@yahoo.co.jp>
2025-11-20 11:04:27 -0800

    feat: Add redirect function in login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,6 +1,13 @@
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+
 import LoginForm from "@/components/LoginForm";
 
-export default function loginPage() {
+export default async function loginPage() {
+  const cookieStore = await cookies();
+  const userId = cookieStore.get("userId");
+  if (userId) redirect("/dashboard");
+
   return (
     <>
       <p>login page</p>
