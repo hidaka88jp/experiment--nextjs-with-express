58baa48aad3b36dfee3da88db054ef5f1d733c35
hidaka88jp <hidaka88jp@yahoo.co.jp>
2025-11-20 10:50:57 -0800

    feat: Add getting user info function

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,15 +2,18 @@ import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 
 import { LogoutButton } from "@/components/LogoutButton";
+import { getUser } from "@/app/lib/getUser";
 
 export default async function DashboardPage() {
   const cookieStore = await cookies();
   const userId = cookieStore.get("userId");
   if (!userId) redirect("/login");
 
+  const user = await getUser(userId.value);
+
   return (
     <div>
-      <p>Welcome to Dashboard</p>
+      <p>Welcome {user.name}</p>
       <LogoutButton />
     </div>
   );
--- app/lib/getUser.ts
+++ app/lib/getUser.ts
@@ -1,6 +1,5 @@
 export async function getUser(userId: string) {
   const res = await fetch(`${process.env.INTERNAL_URL}/users/${userId}`, {
-    // SSR のキャッシュを無効化して毎回最新を取る
     cache: "no-store", 
   });
 
