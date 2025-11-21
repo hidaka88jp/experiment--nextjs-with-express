fbf7e25d901099d007ea699e850e78c9fba12c3d
hidaka88jp <hidaka88jp@yahoo.co.jp>
2025-11-21 13:14:32 -0800

    feat: display message list on top page and integrate PostForm

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 import Link from "next/link";
 import { cookies } from "next/headers";
+import { getAllMessages } from "@/app/lib/getAllMessages";
 
 import { PostForm } from "@/components/PostForm";
+
 
 export default async function Home() {
   const cookieStore = await cookies();
@@ -9,10 +11,17 @@ export default async function Home() {
 
   const isLoggedIn = !!token;
 
+  const messages = await getAllMessages();
+
   return (
     <>
       <p>Message Board</p>
       {isLoggedIn ? <PostForm /> : <Link href="/login">Login</Link>}
+      <ul className="mt-8">
+        {messages.map((msg) => (
+          <li key={msg.id}><b>{msg.user.name}:</b> {msg.message}</li>
+        ))}
+      </ul>
     </>
   );
 }
