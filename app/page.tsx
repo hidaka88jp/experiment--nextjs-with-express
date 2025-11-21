1f97f4111b13aa9c5e389c76b4ca56ceb80f4b2d
hidaka88jp <hidaka88jp@yahoo.co.jp>
2025-11-21 10:01:20 -0800

    feat: show PostForm or login link on top page based on login status

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,16 @@
 import Link from "next/link";
+import { cookies } from "next/headers";
 
-export default function Home() {
+export default async function Home() {
+  const cookieStore = await cookies();
+  const token = cookieStore.get("session_token");
+
+  const isLoggedIn = !!token;
+
   return (
     <>
-      <p>my page</p>
-      <Link href="/login">Login</Link>
+      <p>Message Board</p>
+      {isLoggedIn ? <PostForm /> : <Link href="/login">Login</Link>}
     </>
   );
 }
