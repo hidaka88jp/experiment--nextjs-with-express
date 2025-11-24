02a41593e7397558c59fb3751b3c69e8cb8f4a02
hidaka88jp <hidaka88jp@yahoo.co.jp>
2025-11-24 15:56:06 -0800

    style: improve PostForm UI with new styles

--- components/PostForm/index.tsx
+++ components/PostForm/index.tsx
@@ -21,11 +21,11 @@ export function PostForm() {
   }
 
   return (
-    <form action={handleAction}>
-      <input name="message" type="text" placeholder="Write a message..." />
-      <button type="submit">Post</button>
+    <form action={handleAction} className="flex items-center gap-1">
+      <input name="message" type="text" placeholder="Write a message..." className="border border-zinc-800 rounded-md py-1 px-2 grow" />
+      <button type="submit" className="border bg-zinc-700 border-zinc-700 text-gray-50 px-3 py-1 rounded-md hover:bg-gray-50 hover:text-zinc-800 cursor-pointer">Post</button>
 
-      {error && <p style={{ color: "red" }}>{error}</p>}
+      {error && <p className="text-red-400 text-sm mt-1 w-full">{error}</p>}
     </form>
   );
 }
