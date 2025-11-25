93fa8e71d1f0d8f41221ce347413a27232bba1f4
hidaka88jp <hidaka88jp@yahoo.co.jp>
2025-11-25 11:08:03 -0800

    style: improve RegisterForm and registration page UI

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -11,9 +11,11 @@ export default async function RegisterPage() {
   }
 
   return (
-    <>
-      <p>Register</p>
-      <RegisterForm />
-    </>
+    <div className="px-4 pt-4 pb-5 sm:px-8 sm:pt-9 sm:pb-7">
+      <div className="mx-auto flex w-full max-w-94 sm:max-w-md flex-col">
+        <h2 className="text-xl font-medium text-center border-b-2 border-zinc-600">Register</h2>
+        <RegisterForm />
+      </div>
+    </div>
   );
 }
